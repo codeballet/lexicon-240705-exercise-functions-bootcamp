e6ac5e1cad257abda8a494f3b2ea2b8068ba76b8
stjernholm <stjernholm@msn.com>
2024-07-05 17:47:59 +0200

    feat: Finished exercise 4

--- script.js
+++ script.js
@@ -71,101 +71,105 @@
 ////////////////////////////////////////
 // Exercise 4: Stone, Paper, Scissors //
 ////////////////////////////////////////
-let options = ["stone", "paper", "scissors"];
-let userScore = 0;
-let computerScore = 0;
-let user;
-let computer;
+// let options = ["stone", "paper", "scissors"];
+// let userScore = 0;
+// let computerScore = 0;
+// let user;
+// let computer;
 
-function checkInput(input) {
-    return options.includes(input);
-}
+// function checkInput(input) {
+//     return options.includes(input);
+// }
 
-function computerOption() {
-    return options[Math.floor(Math.random() * 3)];
-}
+// function computerOption() {
+//     return options[Math.floor(Math.random() * 3)];
+// }
 
-function userOption() {
-    let option;
-    cleared = false;
-    while (!cleared) {
-        option = prompt("Choose stone, paper, or scissors");
-        option = option.toLowerCase();
-        cleared = checkInput(option);
-    }
-    return option;
-}
+// function userOption() {
+//     let option;
+//     cleared = false;
+//     while (!cleared) {
+//         option = prompt("Choose stone, paper, or scissors");
+//         option = option.toLowerCase();
+//         cleared = checkInput(option);
+//     }
+//     return option;
+// }
 
-function compare(computer, user) {
-    switch (computer) {
-        case "stone":
-            if (user === options[0]) {
-                console.log("It's a draw");
-            } else if (user === options[1]) {
-                userScore++;
-                console.log("You win this round");
-            } else if (user === options[2]) {
-                computerScore++;
-                console.log("Computer wins this round");
-            }
-            break;
-        case "paper":
-            if (user === options[0]) {
-                computerScore++;
-                console.log("Computer wins this round");
-            } else if (user === options[1]) {
-                console.log("It's a draw");
-            } else if (user === options[2]) {
-                userScore++;
-                console.log("You win this round");
-            }
-            break;
-        case "scissors":
-            if (user === options[0]) {
-                userScore++;
-                console.log("You win this round");
-            } else if (user === options[1]) {
-                computerScore++;
-                console.log("Computer wins this round");
-            } else if (user === options[2]) {
-                console.log("It's a draw");
-            }
-            break;
-        default:
-            console.log("I don't know what happened here!");
-            break;
-    }
-}
+// function compare(computer, user) {
+//     switch (computer) {
+//         case "stone":
+//             if (user === options[0]) {
+//                 console.log("It's a draw");
+//             } else if (user === options[1]) {
+//                 userScore++;
+//                 console.log("You win this round");
+//             } else if (user === options[2]) {
+//                 computerScore++;
+//                 console.log("Computer wins this round");
+//             }
+//             break;
+//         case "paper":
+//             if (user === options[0]) {
+//                 computerScore++;
+//                 console.log("Computer wins this round");
+//             } else if (user === options[1]) {
+//                 console.log("It's a draw");
+//             } else if (user === options[2]) {
+//                 userScore++;
+//                 console.log("You win this round");
+//             }
+//             break;
+//         case "scissors":
+//             if (user === options[0]) {
+//                 userScore++;
+//                 console.log("You win this round");
+//             } else if (user === options[1]) {
+//                 computerScore++;
+//                 console.log("Computer wins this round");
+//             } else if (user === options[2]) {
+//                 console.log("It's a draw");
+//             }
+//             break;
+//         default:
+//             console.log("I don't know what happened here!");
+//             break;
+//     }
+// }
 
-function round(counter) {
-    console.log(`\nRound ${counter}:`);
-    computer = computerOption();
-    user = userOption();
-    console.log(`Computer chose: ${computer}`);
-    console.log(`You chose: ${user}`);
-    compare(computer, user);
-    console.log(`Computer's present score: ${computerScore}`);
-    console.log(`Your present score: ${userScore}`);
-}
+// function round(counter) {
+//     console.log(`\nRound ${counter}:`);
+//     computer = computerOption();
+//     user = userOption();
+//     console.log(`Computer chose: ${computer}`);
+//     console.log(`You chose: ${user}`);
+//     compare(computer, user);
+//     console.log(`Computer's present score: ${computerScore}`);
+//     console.log(`Your present score: ${userScore}`);
+// }
 
-function play() {
-    let noWinner = true;
-    let counter = 1;
-    while (noWinner) {
-        if (computerScore < 3 && userScore < 3) {
-            round(counter);
-        } else if (computerScore === 3) {
-            console.log("\nThe computer won this time.");
-            noWinner = false;
-        } else if (userScore === 3) {
-            console.log("\nCongratulations, you won!");
-            noWinner = false;
-        } else {
-            console.log("\nI'm confused... I don't know who won!");
-            noWinner = false;
-        }
-        counter++;
-    }
-}
+// function play() {
+//     let noWinner = true;
+//     let counter = 1;
+//     while (noWinner) {
+//         if (computerScore < 3 && userScore < 3) {
+//             round(counter);
+//         } else if (computerScore === 3) {
+//             console.log("\nThe computer won this time.");
+//             noWinner = false;
+//         } else if (userScore === 3) {
+//             console.log("\nCongratulations, you won!");
+//             noWinner = false;
+//         } else {
+//             console.log("\nI'm confused... I don't know who won!");
+//             noWinner = false;
+//         }
+//         counter++;
+//     }
+// }
 
-play();
+// play();
+
+/////////////////////////////////
+// Exercise 5: Split the Nota! //
+/////////////////////////////////
